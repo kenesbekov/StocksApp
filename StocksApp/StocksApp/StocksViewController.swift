619cb6a4060232a2a852eb0c012020e16fbee122
kenesbekov <78414696+kenesbekov@users.noreply.github.com>
2022-06-09 04:45:54 +0600

    Lesson 17 live coding

---
+++ StocksApp/StocksApp/StocksViewController.swift
@@ -0,0 +1,54 @@
+//
+//  StocksViewController.swift
+//  StocksApp
+//
+//  Created by Abdurahim on 08.06.2022.
+//
+
+import UIKit
+
+final class StocksViewController: UIViewController {
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorStyle = .none
+        tableView.register(StockCell.self, forCellReuseIdentifier: StockCell.typeName)
+        
+        return tableView
+    }()
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupSubviews()
+        view.backgroundColor = .white
+        tableView.dataSource = self
+    }
+    
+    private func setupSubviews() {
+        view.addSubview(tableView)
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+}
+
+extension StocksViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: StockCell.typeName, for: indexPath) as! StockCell
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+}
+
+extension NSObject {
+    static var typeName: String {
+        String(describing: self)
+    }
+}
